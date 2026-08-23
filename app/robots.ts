2a83cc477ba96697dc178ab8f518dc2ccd03fdad
hyu276 <csquocnguyen@gmail.com>
2026-08-24 02:14:23 +0700

    Simplify robots rules typing

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,9 @@ import { getSeoGlobalSettings } from '@/lib/seo';
 
 export default async function robots():Promise<MetadataRoute.Robots>{
   const seo=await getSeoGlobalSettings();
-  const base:MetadataRoute.Robots['rules'] extends Array<infer R>|infer R ? R : never={userAgent:'*',allow:'/',disallow:['/admin/','/admin.html','/api/seo-manager/']};
-  const aiRules=Object.entries(seo.ai_crawlers||{}).map(([userAgent,allowed])=>allowed?{userAgent,allow:'/'}:{userAgent,disallow:'/'});
-  return {rules:[base,...aiRules],sitemap:[`${seo.site_url.replace(/\/$/,'')}/sitemap.xml`,`${seo.site_url.replace(/\/$/,'')}/image-sitemap.xml`],host:seo.site_url};
+  const rules:NonNullable<MetadataRoute.Robots['rules']>=[
+    {userAgent:'*',allow:'/',disallow:['/admin/','/admin.html','/api/seo-manager/']},
+    ...Object.entries(seo.ai_crawlers||{}).map(([userAgent,allowed])=>allowed?{userAgent,allow:'/'}:{userAgent,disallow:'/'})
+  ];
+  return {rules,sitemap:[`${seo.site_url.replace(/\/$/,'')}/sitemap.xml`,`${seo.site_url.replace(/\/$/,'')}/image-sitemap.xml`],host:seo.site_url};
 }
