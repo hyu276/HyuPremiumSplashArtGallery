58431273572401d31fba0ba17afa6dc563885278
hyu276 <csquocnguyen@gmail.com>
2026-08-24 02:12:06 +0700

    Manage crawler access from SEO manager

--- app/robots.ts
+++ app/robots.ts
@@ -1,10 +1,9 @@
 import type { MetadataRoute } from 'next';
-import { siteUrl } from '@/lib/catalogue';
+import { getSeoGlobalSettings } from '@/lib/seo';
 
-export default function robots():MetadataRoute.Robots{
-  return {
-    rules:[{userAgent:'*',allow:'/',disallow:['/admin/','/admin.html']}],
-    sitemap:[`${siteUrl}/sitemap.xml`,`${siteUrl}/image-sitemap.xml`],
-    host:siteUrl
-  };
+export default async function robots():Promise<MetadataRoute.Robots>{
+  const seo=await getSeoGlobalSettings();
+  const base={userAgent:'*',allow:'/',disallow:['/admin/','/admin.html','/api/seo-manager/']} as const;
+  const aiRules=Object.entries(seo.ai_crawlers||{}).map(([userAgent,allowed])=>({userAgent,allow:allowed?'/':undefined,disallow:allowed?undefined:'/'}));
+  return {rules:[base,...aiRules],sitemap:[`${seo.site_url.replace(/\/$/,'')}/sitemap.xml`,`${seo.site_url.replace(/\/$/,'')}/image-sitemap.xml`],host:seo.site_url};
 }
