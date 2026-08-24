a5440873fed0e6eb4c2a3cfb0e55d2bb3be9f837
hyu276 <csquocnguyen@gmail.com>
2026-08-24 11:44:37 +0700

    chore: bust admin cache for credit picker fix

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -30,7 +30,7 @@
   const cfg=window.HYU_SUPABASE_CONFIG||{};
   const profiles=window.HYU_SUPABASE_PROFILES||{};
   const isAdmin=/\/admin\.html$/i.test(window.location.pathname);
-  const ADMIN_ASSET_VERSION='20260824-basic-guide-1';
+  const ADMIN_ASSET_VERSION='20260824-credit-picker-2';
 
   const authStorageKeys=[];
   for(const profile of Object.values(profiles)){
